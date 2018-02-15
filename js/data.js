738b8e04e29a8478137ca57b8540a00123b0d480
gabrielamf <glennygabb@gmail.com>
2018-02-15 05:44:33 -0500

    añade personajes iconos y estilos

--- js/data.js
+++ js/data.js
@@ -2,42 +2,25 @@ var data = [{
   'id':2,
   'name': 'luke skywalker',
   'image': 'assets/images/luke.png',
-
 },
 {
   'id':5,
   'name': 'leia organa',
   'image': 'assets/images/leia.png',
-
+},
+{
+  'id':4,
+  'name': 'Darth Vader',
+  'image': 'assets/images/vader.png',
+},
+{
+  'id':14,
+  'name': 'Han Solo',
+  'image': 'assets/images/han-solo.png',
+},
+{
+  'id':20,
+  'name': 'Yoda',
+  'image': 'assets/images/yoda.png',
 }
-// {
-//   'name': ' ',
-// },
-// {
-//   'name': ' ',
-// },
-// {
-//   'name': ' ',
-// },
-// {
-//   'name': ' ',
-// },
-// { 
-//   'name': ' ',
-// },
-// {
-//   'name': ' ',
-// },
-// {
-//   'name': ' ',
-// },
-// {
-//   'name': ' ',
-// },
-// {
-//   'name': ' ',
-// },
-// {
-//   'name': ' ',
-// }
 ];
