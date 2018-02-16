120dee5f9778d13d411a72516759abc6cb1c597d
Jennifecarmen <jennifercarmens@gmail.com>
2018-02-16 06:35:44 -0500

    imagenes

--- js/app.js
+++ js/app.js
@@ -3,46 +3,10 @@
   let container = $('#container-imagenes');
   let nresult = $('.noresult');
   nresult.hide();
-  data.forEach(function(element) {
-    if (data.length === 0) {
-      nresult.show();
-    } else {
-      let grupo =
-    '<div class="col-xs-4 photo collection text-center flip" data-name="' + element.name + '" data-id="' + element.id + '">' +
-    '<img class="img-responsive flip-1 content" src = ' + element.image + ' data-target="#imgModal" data-toggle="modal">' +
-    '</div>';
-      container.append(grupo);
-    }
-  });
+  const proxy = 'https://cors-anywhere.herokuapp.com/';
 
-  // filtro
-  $('#search').keyup(function() {
-    let name = $(this).val().toLowerCase();
-    $('.collection').hide();
-    $('.collection').each(function() {
-      let search = $(this).data('name');
-      let id = $(this).data('id');
-      if (search.indexOf(name) !== -1) {
-        $(this).show();
-      }
-    });
-  });
-
-  // modal
-  $('.collection').click(function() {
-    console.log(event.target);
-    let name = $(this).data('name');
-    let id = $(this).data('id');
-
-    $('.name').text(name);
-    const url = `https://swapi.co/api/people/${id}?format=json`;
-    fetch(url)
-      .then(handleErrors)
-      .then(parseJSON)
-      .then(addInfo)
-      .catch(displayErrors);
-  });
-
+  const urlpeople = 'https://swapi.co/api/people?format=json';
+ 
   const handleErrors = (res) => {
     if (!res.ok) {
       throw Error(res.status);
@@ -56,19 +20,65 @@
         return parsedData;
       });
   };
+  const displayErrors = (err) => {
+    console.log('INSIDE displayErrors!');
+    console.log(err);
+  };
+  const addImages = (data) => {
+    let quantity = data.count;
+    for (let i = 1;i < quantity;i++) {
+      if (i === 17) {
+ continue; 
+}
+      let grupo =
+          '<div class="col-xs-4 photo collection text-center flip" id="' + i + '"  data-id="' + i + '" >' +
+          `<img class="img-responsive flip-1 content" src = https://starwars-visualguide.com/assets/img/characters/${i}.jpg data-target="#imgModal" data-toggle="modal">` +
+          '</div>';
+      container.append(grupo);
+    }
+  };
+  fetch(proxy + urlpeople)
+    .then(handleErrors)
+    .then(parseJSON)
+    .then(addImages)
+    .catch(displayErrors);
 
-  const addInfo = (data) => {
-    let height = data.height;
-    let hairColor = data.hair_color;
-    let eyeColor = data.eye_color;
-    let mass = data.mass;
+
+  // filtro
+
+  // modal
+  $(document).on('click', '.collection', function() {
+    let id = $(this).data('id');
+    const url = `https://swapi.co/api/people/${id}?format=json`;
+    $.ajax({
+      url: url,
+      success: addInfo, search
+    });
+  });
+
+  const addInfo = (res) => {
+    let name = res.name;
+    let height = res.height;
+    let hairColor = res.hair_color;
+    let eyeColor = res.eye_color;
+    let mass = res.mass;
+    $('.name').text(name);
     $('.hair_color').text(hairColor);
     $('.height').text(height);
     $('.eye_color').text(eyeColor);
     $('.mass').text(mass);
   };
-  const displayErrors = (err) => {
-    console.log('INSIDE displayErrors!');
-    console.log(err);
+  const search = (respons) => {
+    // $('#search').keyup(function() {
+    //   let name = $(this).val().toLowerCase();
+    //   $('.collection').hide();
+    //   $('.collection').each(function() {
+    //     let search = res.name;
+    //     if (search.indexOf(name) !== -1) {
+    //       $(this).show();
+    //     }
+    //   });
+    // });
   };
+  search();
 })();
